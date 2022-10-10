a87e5db1febaa0c639452f75c238d6722a2ca86d
Readek <readeku@gmail.com>
2022-10-10 20:12:55 +0200

    Add Bracket html (with placeholder data)

---
+++ Stream Tool/Resources/Scripts/Bracket.js
@@ -0,0 +1,187 @@
+'use strict';
+
+let bracketData;
+const playerData = [];
+const playerSize = '28px';
+const tagSize = '16px';
+const fadeOutTime = .3
+const fadeInTime = .3
+
+class BracketPlayer {
+
+    constructor(round, pos) {
+
+        this.round = round;
+        this.pos = pos;
+
+        // assign the html elements to variables
+        this.nameEl = document.getElementById(round).getElementsByClassName("playerName")[pos];
+        this.tagEl = document.getElementById(round).getElementsByClassName("playerTag")[pos];
+        this.charEl = document.getElementById(round).getElementsByClassName("playerIcon")[pos];
+        this.scoreEl = document.getElementById(round).getElementsByClassName("score")[pos];
+    
+    }
+
+    update() {
+
+        // text update
+        if (this.nameEl.innerHTML != bracketData[this.round][this.pos].player ||
+            this.tagEl.innerHTML != bracketData[this.round][this.pos].tag) {
+
+            fadeOut(this.nameEl.parentElement).then( () => {
+
+				this.nameEl.style.fontSize = playerSize;
+                this.nameEl.innerHTML = bracketData[this.round][this.pos].player;
+                this.tagEl.style.fontSize = tagSize;
+                this.tagEl.innerHTML = bracketData[this.round][this.pos].tag;
+
+                // remove tag from flow if not visible
+                if (this.tagEl.innerHTML == "") {
+                    this.tagEl.style.display = "none";
+                    this.tagEl.parentElement.style.transform = "translate(3px, 0px)";
+                } else {
+                    this.tagEl.style.display = "block";
+                    this.tagEl.parentElement.style.transform = "translate(3px, -4px)";
+                }
+
+                resizeText(this.nameEl.parentElement);
+				fadeIn(this.nameEl.parentElement);
+
+			});
+
+        }
+
+        // score update
+        if (this.scoreEl.innerHTML !== bracketData[this.round][this.pos].score) {
+
+            this.scoreEl.innerHTML = bracketData[this.round][this.pos].score;
+            // if more or less score than the other player
+            const rivalEncounter = this.pos % 2 ? this.pos-1 : this.pos+1;
+            if (this.scoreEl.innerHTML > bracketData[this.round][rivalEncounter].score) {
+                this.nameEl.parentElement.style.color = "#90ffb1";
+                this.charEl.style.filter = "grayscale(0)"
+            } else if (this.scoreEl.innerHTML == bracketData[this.round][rivalEncounter].score) {
+                this.nameEl.parentElement.style.color = "white";
+                this.charEl.style.filter = "grayscale(0)"
+            } else {
+                this.nameEl.parentElement.style.color = "#ffa3a3";
+                this.charEl.style.filter = "grayscale(1)"
+            }
+
+        }
+
+        // character update
+        if (!this.charEl.src.includes(bracketData[this.round][this.pos].character)) {
+            fadeOut(this.charEl).then( () => {
+                this.charEl.src = `Resources/Characters/${bracketData[this.round][this.pos].character}.png`;
+                // hide character icon if none
+                if (bracketData[this.round][this.pos].character == "None") {
+                    this.charEl.style.display = "none";
+                } else {
+                    this.charEl.style.display = "block";
+                }
+                fadeIn(this.charEl);
+            });
+        }
+        
+
+    }
+
+};
+
+
+// and here is where we add all the player references
+playerData.push(
+    new BracketPlayer("WinnersSemis", 0),
+    new BracketPlayer("WinnersSemis", 1),
+    new BracketPlayer("WinnersSemis", 2),
+    new BracketPlayer("WinnersSemis", 3),
+    new BracketPlayer("WinnersFinals", 0),
+    new BracketPlayer("WinnersFinals", 1),
+    new BracketPlayer("GrandFinals", 0),
+    new BracketPlayer("GrandFinals", 1),
+    new BracketPlayer("TrueFinals", 0),
+    new BracketPlayer("TrueFinals", 1),
+    new BracketPlayer("LosersTop8", 0),
+    new BracketPlayer("LosersTop8", 1),
+    new BracketPlayer("LosersTop8", 2),
+    new BracketPlayer("LosersTop8", 3),
+    new BracketPlayer("LosersQuarters", 0),
+    new BracketPlayer("LosersQuarters", 1),
+    new BracketPlayer("LosersQuarters", 2),
+    new BracketPlayer("LosersQuarters", 3),
+    new BracketPlayer("LosersSemis", 0),
+    new BracketPlayer("LosersSemis", 1),
+    new BracketPlayer("LosersFinals", 0),
+    new BracketPlayer("LosersFinals", 1)
+)
+
+
+// main loop
+async function updateData() {
+
+    // actual update
+	bracketData = await getInfo();
+    for (let i = 0; i < playerData.length; i++) {
+        playerData[i].update();
+    }
+
+    // if true finals players exist, show true finals round
+    if (playerData[8].nameEl.innerText || playerData[9].nameEl.innerText) {
+        if (window.getComputedStyle(document.getElementById("TrueFinals")).getPropertyValue("display") == "none") {
+            document.getElementById("TrueFinals").style.display = "flex";
+            resizeText(playerData[8].nameEl);
+            resizeText(playerData[9].nameEl);
+        }
+    } else {
+        document.getElementById("TrueFinals").style.display = "none";
+    }
+    
+}
+updateData();
+setInterval( () => { updateData() }, 1000); // update interval
+
+
+// searches for the main json file
+function getInfo() {
+	return new Promise(function (resolve) {
+		const oReq = new XMLHttpRequest();
+		oReq.addEventListener("load", reqListener);
+		oReq.open("GET", 'Resources/Texts/Bracket.json');
+		oReq.send();
+
+		//will trigger when file loads
+		function reqListener () {
+			resolve(JSON.parse(oReq.responseText))
+		}
+	})
+}
+
+
+// text resize, keeps making the text smaller until it fits
+function resizeText(textEL) {
+	const childrens = textEL.children;
+	while (textEL.scrollWidth > textEL.offsetWidth) {
+		if (childrens.length > 0) { //for tag+player texts
+			Array.from(childrens).forEach((child) => {
+				child.style.fontSize = getFontSize(child);
+			});
+		} else {
+			textEL.style.fontSize = getFontSize(textEL);
+		}
+	}
+}
+// returns a smaller fontSize for the given element
+function getFontSize(textElement) {
+	return (parseFloat(textElement.style.fontSize.slice(0, -2)) * .90) + 'px';
+}
+
+// animations
+async function fadeOut(itemID, dur = fadeOutTime) {
+	itemID.style.animation = `fadeOut ${dur}s both`;
+	// this function will return a promise when the animation ends
+	await new Promise(resolve => setTimeout(resolve, dur * 1000)); // translate to miliseconds
+}
+function fadeIn(itemID, delay = 0, dur = fadeInTime) {
+	itemID.style.animation = `fadeIn ${dur}s ${delay}s both`;
+}
