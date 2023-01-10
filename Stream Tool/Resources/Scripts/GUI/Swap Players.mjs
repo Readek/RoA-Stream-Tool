d8c9de87573960c7e71d53b5352fd2091f7f79cf
Readek <readeku@gmail.com>
2023-01-10 14:09:04 +0100

    The End of Swap Bugs

--- Stream Tool/Resources/Scripts/GUI/Swap Players.mjs
+++ Stream Tool/Resources/Scripts/GUI/Swap Players.mjs
@@ -1,9 +1,13 @@
-import { players } from "./Player/Players.mjs";
+import { players, playersReady } from "./Player/Players.mjs";
 import { scores } from "./Scores.mjs";
 import { teams } from "./Teams.mjs";
 import { wl } from "./WinnersLosers.mjs";
 
-document.getElementById('swapButton').addEventListener("click", swapPlayers);
+document.getElementById('swapButton').addEventListener("click", () => {
+    if (playersReady()) {
+        swapPlayers();
+    }
+});
 
 export async function swapPlayers() {
     
