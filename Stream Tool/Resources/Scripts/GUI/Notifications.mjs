d0c72a0cbf99feb41d19576d6a51e99e70c4e552
Readek <readeku@gmail.com>
2023-03-12 14:33:21 +0100

    Allow more than one notification at the same time

--- Stream Tool/Resources/Scripts/GUI/Notifications.mjs
+++ Stream Tool/Resources/Scripts/GUI/Notifications.mjs
@@ -1,16 +1,28 @@
-const notifSpan = document.getElementById("notifText");
+const notifDiv = document.getElementById("notifDiv");
 
 /**
  * Displays a notification on screen
  * @param {String} text - Text to be shown
  */
 export function displayNotif(text) {
-    
-    notifSpan.innerHTML = text;
 
-    notifSpan.style.animation = "";
-    setTimeout(() => {
-        notifSpan.style.animation = "notifAnim 2.5s both";
-    });
+    // create the element and give it some style
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("notifText");
+    
+    // give it that text
+    newDiv.innerHTML = text;
+
+    // add it to the notification area
+    notifDiv.appendChild(newDiv);
+
+    // when the animation finishes, kill the element
+    newDiv.addEventListener("animationend", killNotif);
+
+}
+
+function killNotif() {
+    
+    notifDiv.removeChild(this);
 
 }
