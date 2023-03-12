881f27f7d8b946de76a43d59eb5f8720b3d14742
Readek <readeku@gmail.com>
2023-03-12 13:02:27 +0100

    Extended example plugin

--- Stream Tool/Resources/Scripts/GUI Plugins/Example.mjs
+++ Stream Tool/Resources/Scripts/GUI Plugins/Example.mjs
@@ -1,7 +1,22 @@
 import { displayNotif } from "../GUI/Notifications.mjs";
+import { players } from "../GUI/Player/Players.mjs";
+import { tournament } from "../GUI/Tournament.mjs";
 
 // this is just a regular simple plugin example
+// you can add any kind of js code here, feel free to go wild
 // uncomment the following lines to see it in action
-/* setTimeout(() => {
-    displayNotif("Example plugin has loaded!");
-}, 1000); */
+/* 
+
+players[0].setName("Plugin Test 1");
+await players[0].charChange("Zetterburn");
+
+players[1].setName("Plugin Test 2");
+await players[1].charChange("Mollo", true);
+await players[1].skinChange(players[1].findSkin("Champion"));
+
+tournament.setText("Example plugin is ON");
+
+
+displayNotif("Example plugin has loaded!");
+
+*/
